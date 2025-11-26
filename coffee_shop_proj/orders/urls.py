c206dc2e8334eb53db61aff6ecea858f70312426
samuelhurtado20 <samuelhurtado20@hotmail.com>
2025-11-25 22:52:46 -0500

    Refactor order item creation and update product list template for improved functionality

--- coffee_shop_proj/orders/urls.py
+++ coffee_shop_proj/orders/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from orders.views import OrderView
+from orders.views import CreateOrdenItemView, OrderView
 
 """ from orders.views import OrderListView, OrderFormView """
 
 urlpatterns = [
     path('detail/', OrderView.as_view(), name='my_order'),
-    path('item/', OrderView.as_view(), name='new_item'),
+    path('item/', CreateOrdenItemView.as_view(), name='add_item'),
 ]
--- coffee_shop_proj/orders/views.py
+++ coffee_shop_proj/orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import OrderItemForm
 from orders.models import Order, OrderItem
 
 
@@ -15,7 +16,16 @@ class OrderView(LoginRequiredMixin, DetailView):
 
 
 class CreateOrdenItemView(LoginRequiredMixin, CreateView):
-    model = OrderItem
+    # model = OrderItem
     template_name = "orders/new_item.html"
-    fields = ["product", "quantity"]
-    success_url = reverse_lazy("orders:order")
+    form_class = OrderItemForm
+    # fields = ["product", "quantity"]
+    success_url = reverse_lazy("my_order")
+
+    def form_valid(self, form):
+        # order_item = form.save(commit=False)
+        order, _ = Order.objects.get_or_create(is_active=True, user=self.request.user)
+        form.instance.order = order
+        form.instance.quantity = 1
+        form.save()
+        return super().form_valid(form)
