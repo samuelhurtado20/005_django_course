b8aeabe077bd4db765a11d1e986062e2e84608d1
samuelhurtado20 <samuelhurtado20@hotmail.com>
2025-11-27 19:12:32 -0500

    Update database settings in settings.py to use PostgreSQL

--- coffee_shop_proj/coffee_shop_proj/settings.py
+++ coffee_shop_proj/coffee_shop_proj/settings.py
@@ -85,23 +85,23 @@ WSGI_APPLICATION = "coffee_shop_proj.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "coffee_shop_db",
-#         "USER": "postgres",
-#         "PASSWORD": "your_password",
-#         "HOST": "localhost",
-#         "PORT": "5432",
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "coffee_shop_db",
+        "USER": "mi_usuario",
+        "PASSWORD": "mi_contraseña",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
 
 
 # Password validation
